f9bedb82fde8658c7ab911450e6c648404bcd300
Adib Grouz <contact@adib-grouz.com>
2024-09-26 21:26:14 +0200

    update deprecated method utcnow to now

--- app/core/services/auth.py
+++ app/core/services/auth.py
@@ -28,9 +28,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     """
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
