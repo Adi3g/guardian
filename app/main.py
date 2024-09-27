80e36fd35876256df11868448c3d8258c949ca99
Adib Grouz <contact@adib-grouz.com>
2024-09-27 15:38:57 +0200

    add metrics endpoint

--- app/main.py
+++ app/main.py
@@ -3,10 +3,26 @@ from __future__ import annotations
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from prometheus_client import generate_latest
 
 from app.interfaces.api import router
+from app.middlewares.metrics_middleware import MetricsMiddleware
 
 app = FastAPI(title='Guardian Security Gateway')
+
+# Add MetricsMiddleware to capture metrics on every request
+app.add_middleware(MetricsMiddleware)
+
+
+# Prometheus Metrics endpoint
+@app.get('/metrics')
+def metrics():
+    """
+    Endpoint to expose Prometheus metrics for scraping.
+    """
+    return PlainTextResponse(generate_latest())
+
 
 # Include the API router
 app.include_router(router)
